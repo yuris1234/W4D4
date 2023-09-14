7b6b6abe44c158879b75a9938c882ebc6c7be2b1
Jason Zhang <jasonzhang501@gmail.com>
2023-09-14 13:47:46 -0400

    Started First Anagram

---
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -0,0 +1,13 @@
+def first_anagram?(string1,string2)
+    split = string1.split
+    perms = split.permutation.to_a
+
+    perms = perms.map do |ele|
+        ele.join('')
+    end
+
+    perms.include?(string2)
+end
+
+p first_anagram?("gizmo", "sally")   
+p first_anagram?("elvis", "lives") 
