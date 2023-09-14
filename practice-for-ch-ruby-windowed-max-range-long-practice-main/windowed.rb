3336046a01a5c3243ab8d426d07eacf07d0e72d0
yuris <yls4@cornell.edu>
2023-09-14 16:27:08 -0400

    finished windowed max range

--- practice-for-ch-ruby-windowed-max-range-long-practice-main/windowed.rb
+++ practice-for-ch-ruby-windowed-max-range-long-practice-main/windowed.rb
@@ -1,23 +1,27 @@
+require "byebug"
+
 def windowed_max_range(array,window_size)
     current_max_range = 0
     holder = []
 
-    array.each_with_index do |ele,i|
+    i = 0
+    while i <= array.length
         if holder.length < window_size
-            holder << ele
+            holder << array[i]
+            i += 1
         else
             difference = holder.max - holder.min
             if difference > current_max_range
                 current_max_range = difference
             end
-            holder = []
+            holder.shift
         end
     end
     
-    current_max_range
+    return current_max_range
 end
 
-p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
-p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
-p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
-p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) # 3, 2, 5, 4, 8
