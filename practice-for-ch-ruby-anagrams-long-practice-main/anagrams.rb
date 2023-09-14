15de78e2e5f700da84f0ed0abe6982e7ec9cfc58
yuris <yls4@cornell.edu>
2023-09-14 14:09:45 -0400

    finished first two anagrams

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -12,3 +12,14 @@ end
 # p first_anagram?("gizmo", "sally")   
 # p first_anagram?("elvis", "lives") 
 
+def second_anagram?(string1, string2) # O(n) + O(n) + O(n) + O(n) => O(n)
+    split = string2.split("")
+    string1.each_char do |char|
+        idx = split.find_index(char)
+        split.delete_at(idx) if idx != nil
+    end
+    split.empty?
+end
+
+# p second_anagram?("gizmo", "sally")   
+# p second_anagram?("elvis", "lives") 
