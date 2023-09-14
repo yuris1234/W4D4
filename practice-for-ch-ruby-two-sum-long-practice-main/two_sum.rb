25c38f2deb900573620205e419ece4fda50b605b
yuris <yls4@cornell.edu>
2023-09-14 14:46:50 -0400

    finished okay two sum

---
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -0,0 +1,51 @@
+def bad_two_sum?(arr, target_sum) # O(n**2)
+    arr.each_with_index do |ele1, idx1|
+        arr.each_with_index do |ele2, idx2|
+            if idx2 > idx1 
+                return true if ele1 + ele2 == target_sum
+            end
+        end
+    end
+    false             
+end
+
+def okay_two_sum(arr, target_sum) # O(n log n) + O(n) => O(n)
+    arr = merge_array(arr)
+    i = 0
+    j = arr.length - 1
+    while i < j
+        sum = arr[i] + arr[j]
+        return true if arr[i] + arr[j] == target_sum 
+        if sum > target_sum
+            j -= 1
+        else
+            i += 1
+        end
+    end
+    false
+end
+
+def merge_array(arr)
+    return arr if arr.length <= 1
+
+    middle = arr.size / 2
+    left = arr[0...middle]
+    right = arr[middle..-1]
+
+    merge(merge_array(left), merge_array(right))
+end
+
+def merge(left, right)
+    holder = []
+    until left.empty? || right.empty?
+        holder << (left[0] > right[0] ? right.shift : left.shift)
+    end
+    holder + left + right
+end
+
+arr = [0, 1, 5, 7]
+# arr = [3,5,1,4,7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+
+# p okay_two_sum(arr, 5)
