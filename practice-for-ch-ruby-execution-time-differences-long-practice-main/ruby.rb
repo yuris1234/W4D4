37a5a869b003a75ac40cbf201d3cd0d80bd07621
yuris <yls4@cornell.edu>
2023-09-14 12:47:45 -0400

    completed contiguous subsums

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/ruby.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/ruby.rb
@@ -37,10 +37,31 @@ end
 def faster_subsum(list)
     largest_sum = 0
     current_sum = 0
-    (0...list.length).each do |i|
-        if list[i]
+
     list.each do |ele|
-        current_sum += ele 
-        if current_sum > largest_sum 
-            largest_sum = current_sum
+        if ele > 0
+            current_sum += ele
+            if current_sum > largest_sum
+                largest_sum = current_sum
+            end
+        elsif ele <= 0
+            if current_sum > largest_sum
+                largest_sum = current_sum
+            end
+            current_sum = 0
+        end
+    end
+    if current_sum == 0
+        largest_sum = list[0]
+        list.each do |ele|
+            if ele > largest_sum
+                largest_sum = ele 
+            end
+        end
+    end
+    largest_sum
 end
+
+p faster_subsum([-1,-2,-3])
+p faster_subsum([1,2,4,-1,5,1,-1,9,10])
+p faster_subsum([1,-1,2,-1,2,0,0,9])
